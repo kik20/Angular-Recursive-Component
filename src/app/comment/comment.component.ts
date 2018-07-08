5b7fbd01a0cc87d45c943eaa6e2e1260cbee9092
Kislay Kaushal <kislay.kaushal20@gmail.com>
2018-07-08 10:41:18 +0530

    Css color changes for better display

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -4,14 +4,16 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css'],
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
+  // All the child comments
   commentReplies: string[] = [];
   @Input()
   childOrder: number;
   commentNumber: number;
   animate = false;
+  // Event to notify all parents on button hover
   @Output()
   childEvent = new EventEmitter<boolean>();
   constructor(private commentService: CommentService) { }
@@ -32,6 +34,12 @@ export class CommentComponent implements OnInit {
     this.childEvent.emit(false);
   }
 
+  /**
+   * Receive child button hover event from children comments and pass on to its parents
+   *
+   * @param {*} event
+   * @memberof CommentComponent
+   */
   onReceiveChildEvent(event) {
     this.animate = event;
     this.childEvent.emit(event);
